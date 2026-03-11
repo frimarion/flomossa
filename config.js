ffb1124a36e2e16db5c4c63ca9d1b77b2e268059
frimarion <fr34m1337@gmail.com>
2026-03-11 15:35:50 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   BOT_TOKEN:     "8625314910:AAHl3IOiJQADD7BsEC776-wIbkCooWFJJCo",   // токен от @BotFather
-  ADMIN_CHAT_ID: 2647751769,            // ваш Telegram ID (от @userinfobot)
+  ADMIN_CHAT_ID: 264775176,            // ваш Telegram ID (от @userinfobot)
   MINI_APP_URL:  "https://frimarion.github.io/flomossa/",     // URL где будет лежать index.html (см. инструкцию)
 };
